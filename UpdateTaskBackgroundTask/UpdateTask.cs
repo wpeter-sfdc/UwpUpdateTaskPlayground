bb6e38b0399fee894c0f5b1a44bbc114cc869734
Wesley Peter <wpeter@salesforce.com>
2017-09-13 15:04:18 -0700

    add solution

---
+++ UpdateTaskBackgroundTask/UpdateTask.cs
@@ -0,0 +1,13 @@
+﻿using System.Diagnostics;
+using Windows.ApplicationModel.Background;
+
+namespace UpdateTaskBackgroundTask
+{
+    public sealed class UpdateTask : IBackgroundTask
+    {
+        public void Run(IBackgroundTaskInstance taskInstance)
+        {
+            Debugger.Break();
+        }
+    }
+}
